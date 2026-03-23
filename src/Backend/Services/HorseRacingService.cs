df1fdf0fd407ca761e9a980407fa8e9586a98960
Sumeya <SM3949@live.mdx.ac.uk>
2026-03-23 17:01:04 +0000

    backend hash table structure and services

---
+++ src/Backend/Services/HorseRacingService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sports_betting.Services
+{
+    public class HorseRacingService
+    {
+        public List<(string RaceName, double Odds)> GetEvents()
+        {
+            return new List<(string, double)>
+            {
+                ("Royal Ascot - Thunderbolt", 3.20),
+                ("Cheltenham - Night Runner", 4.50),
+                ("Epsom Derby - Silver Arrow", 2.80)
+            };
+        }
+    }
+}
